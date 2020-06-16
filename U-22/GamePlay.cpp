bccca15be508b0dcb91bf661acecaed0437aabdb
HachimineDaiki <daiki-hachimine-19h@kbc.ac.jp>
2020-06-16 13:28:54 +0900

    プレイヤー弾挙動のみ

--- U-22/GamePlay.cpp
+++ U-22/GamePlay.cpp
@@ -1,6 +1,17 @@
 #include "DxLib.h"
 #include "GamePlay.h"
-#include  "KeyControl.h"
+#include "KeyControl.h"
+
+
+int i;
+int x = 0;
+int x2 =600;
+int y = 500;
+int y2 = 600;
+int x3 = 0;
+bool Flg = FALSE;
+
+
 
 void GamePlay() {
 	PlayDisp();
@@ -8,10 +19,12 @@ void GamePlay() {
 }
 
 void PlayDisp() {
-	DrawBox(500, 500, 600, 600, 0xFF0000, TRUE);
+	DrawBox(500, 600, 600, 700, 0xFF0000, true);
 }
+
 void PlayMove() {
-	if (g_KeyFlg & KEY_INPUT_Z) {
-		DrawBox(500, 500,650, 650, 0xFF0000, TRUE);
-	}
+	if (g_KeyFlg & KEY_INPUT_Z) Flg = TRUE;
+	if (Flg == TRUE) x3 += 5;
+	if (x3 > 500) Flg = TRUE, x = 0;
+	if (Flg == TRUE)DrawBox(500 + x3, 500, 550 + x3, 550, 0xFFFFFF, TRUE);
 }
